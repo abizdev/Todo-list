0c8d91f8c910ea01f432c9d4268ad8cafa014749
Abdulaziz <abizlancer@gmail.com>
2023-06-14 20:45:21 +0500

    part 1

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <nav class="navbar">
+    <div class="navbar-nav container">
+      <button class="navbar-btn btn-lang">Ru</button>
+      <h1 class="navbar-nav__title">Заметки</h1>
+      <button class="navbar-btn btn-search">
+        <img src="@/assets/images/search.png" alt="search">
+      </button>
+    </div>
+    <!-- <div class="navbar-search container">
+      <form class="navbar-search__form">
+        <button class="navbar-btn back">
+          <img src="@/assets/images/back.svg" alt="">
+        </button>
+        <input type="text" placeholder="Поиск..." class="navbar-search__input">
+        <button class="navbar-btn claer">
+          <img src="@/assets/images/clear.svg" alt="">
+        </button>
+      </form>
+    </div> -->
+  </nav>
+  <section class="content"></section>
+  <div class="modal"></div>
+  <button class="add-item"></button>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
+
+<style>
+  
+</style>
