33158d41c9939dfdca06d5856fe37d36efaa5d0d
Abdulaziz <abizlancer@gmail.com>
2023-06-22 14:44:47 +0500

    part 3

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,39 @@
 <template>
   <navbar />
   <content-items />
-  
-  <div class="modal"></div>
-  <button class="add-item"></button>
+  <modal v-show="openOrCloseM" @closeModal="closeModal" @addItem="addItem" />
+  <add-btn @openModal="openModal" />
 </template>
 
 <script>
 import Navbar from './components/Navbar.vue';
 import ContentItems from './components/Contentitems.vue';
+import Modal from './components/Modal.vue';
+import AddBtn from './components/AddBtn.vue';
 
 export default {
   components: {
     Navbar,
-    ContentItems
-  }
+    ContentItems,
+    Modal,
+    AddBtn,
+  },
+  data() {
+    return {
+      openOrCloseM: false
+    }
+  },
+  methods: {
+    openModal(bool) {
+      this.openOrCloseM = bool
+    },
+    closeModal(bool) {
+      this.openOrCloseM = bool
+    },
+    addItem(itemObj) {
+      console.log(itemObj);
+    }
+  },
 }
 </script>
 
---
+++ src/components/Modal.vue
@@ -0,0 +1,50 @@
+<template >
+  <Transition name="scale">
+    <div class="modal" @click="closeModal">
+      <div class="modal__content" @click.stop>
+        <h4 class="modal__content--title">Добавить заметку</h4>
+        <label class="modal__content--input">
+          <span>Title</span>
+          <input type="text" placeholder="Title" v-model="title">
+        </label>
+        <label class="modal__content--input">
+          <span>Content</span>
+          <input type="text" placeholder="Content" v-model="desc">
+        </label>
+        <div class="modal__content--btns">
+          <button class="modal-btn btn-add" @click="closeModal">Отмена</button>
+          <button class="modal-btn btn-cancel" @click="addItem">Добавить</button>
+        </div>
+      </div>
+    </div>
+  </Transition>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      title: '',
+      desc: '',
+    }
+  },
+  methods: {
+    closeModal() {
+      this.$emit('closeModal', false)
+      this.title = ''
+      this.desc = ''
+    },
+    addItem() {
+      if(this.title != '' && this.desc != '') {
+        const item = {
+          id: 1,
+          title: this.title,
+          desc: this.desc
+        }
+        this.$emit('addItem', item)
+        this.closeModal()
+      }
+    }
+  },
+}
+</script>
